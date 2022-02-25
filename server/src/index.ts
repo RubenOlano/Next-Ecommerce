b63ea742929c575159b2275b9a4b32b5c18b5e6a
Ruben Olano <rubendavid2711@gmail.com>
2022-02-25 15:40:22 -0600

    update backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,14 @@ const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
   apiVersion: "2020-08-27",
 });
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.post("/payment", async (req, res) => {
@@ -27,5 +27,5 @@ app.post("/payment", async (req, res) => {
   const response = await stripe.charges.create(body);
   if (response.status == "failed")
     return res.status(500).send({ errror: response.failure_message });
-  return res.status(200).send({ success: response.invoice });
+  return res.status(200).send({ success: response });
 });
